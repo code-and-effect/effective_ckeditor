5571d3d9fb8cc48ea41b70028d742d96c58989d1
Matt Riemer <matthew@agilestyle.com>
2014-03-28 14:15:24 -0600

    Deal with digested assets in production

--- lib/effective_ckeditor/engine.rb
+++ lib/effective_ckeditor/engine.rb
@@ -2,5 +2,20 @@ module EffectiveCkeditor
   class Engine < ::Rails::Engine
     engine_name 'effective_ckeditor'
     isolate_namespace EffectiveCkeditor
+
+    # Append some precompiled assets we need access to
+    # This works in conjunction with the effective_ckeditor rake task that enhances assets:precompile
+    initializer "effective_ckeditor.append_precompiled_assets" do |app|
+      precompile = [
+        'effective_ckeditor.js',
+        'effective_ckeditor.css',
+        'effective_ckeditor/config.js',
+        'ckeditor/*',
+        'effective/snippets/*'
+      ]
+
+      Rails.application.config.assets.precompile += precompile
+    end
+
   end
 end
