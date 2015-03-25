5f7fd25f6812910501e3f1e05d8964b1fa58c2b9
Matt Riemer <matthew@agilestyle.com>
2015-03-25 15:16:09 -0600

    Replace all image paths with proper Rails asset paths

--- app/assets/javascripts/after_update.sh
+++ app/assets/javascripts/after_update.sh
@@ -2,9 +2,62 @@
 for i in ./ckeditor/skins/moonocolor/*.css; do
   mv $i $i.erb
 
-  search="url(icons.png)"
-  replace="url(<%= asset_path('ckeditor/skins/moonocolor/icons.png') %>)"
+  ### Regular DPI
 
+  # Icons
+  search="url(icons.png)"
+  replace="url(<%= asset_path('ckeditor\\/skins\\/moonocolor\\/icons.png') %>)"
   sed -i '' -e "s/${search}/${replace}/g" "${i}.erb"
+
+  # Arrow.png
+  search="url(images\\/arrow.png)"
+  replace="url(<%= asset_path('ckeditor\\/skins\\/moonocolor\\/images\\/arrow.png') %>)"
+  sed -i '' -e "s/${search}/${replace}/g" "${i}.erb"
+
+  # Close.png
+  search="url(images\\/close.png)"
+  replace="url(<%= asset_path('ckeditor\\/skins\\/moonocolor\\/images\\/close.png') %>)"
+  sed -i '' -e "s/${search}/${replace}/g" "${i}.erb"
+
+  # Lock-open.png
+  search="url(images\\/lock-open.png)"
+  replace="url(<%= asset_path('ckeditor\\/skins\\/moonocolor\\/images\\/lock-open.png') %>)"
+  sed -i '' -e "s/${search}/${replace}/g" "${i}.erb"
+
+  # Lock.png
+  search="url(images\\/lock.png)"
+  replace="url(<%= asset_path('ckeditor\\/skins\\/moonocolor\\/images\\/lock.png') %>)"
+
+  # Refresh.png
+  search="url(images\\/refresh.png)"
+  replace="url(<%= asset_path('ckeditor\\/skins\\/moonocolor\\/images\\/refresh.png') %>)"
+
+  ### Hi DPI
+
+  # Icons
+  search="url(icons_hidpi.png)"
+  replace="url(<%= asset_path('ckeditor\\/skins\\/moonocolor\\/icons_hidpi.png') %>)"
+  sed -i '' -e "s/${search}/${replace}/g" "${i}.erb"
+
+  # Close.png
+  search="url(images\\/hidpi\\/close.png)"
+  replace="url(<%= asset_path('ckeditor\\/skins\\/moonocolor\\/images\\/hidpi\\/close.png') %>)"
+  sed -i '' -e "s/${search}/${replace}/g" "${i}.erb"
+
+  # Lock-open.png
+  search="url(images\\/hidpi\\/lock-open.png)"
+  replace="url(<%= asset_path('ckeditor\\/skins\\/moonocolor\\/images\\/hidpi\\/lock-open.png') %>)"
+  sed -i '' -e "s/${search}/${replace}/g" "${i}.erb"
+
+  # lock.png
+  search="url(images\\/hidpi\\/lock.png)"
+  replace="url(<%= asset_path('ckeditor\\/skins\\/moonocolor\\/images\\/hidpi\\/lock.png') %>)"
+  sed -i '' -e "s/${search}/${replace}/g" "${i}.erb"
+
+  # refresh.png
+  search="url(images\\/hidpi\\/refresh.png)"
+  replace="url(<%= asset_path('ckeditor\\/skins\\/moonocolor\\/images\\/hidpi\\/refresh.png') %>)"
+  sed -i '' -e "s/${search}/${replace}/g" "${i}.erb"
+
 done
 
